d8b2f58cfe60cbca921b1980a80aa208f31451d7
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2022-10-13 19:54:33 -0500

    Mejoras en el astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://pargara.me',
-	base: '/Blog'
 	integrations: [mdx(), sitemap()],
 });
